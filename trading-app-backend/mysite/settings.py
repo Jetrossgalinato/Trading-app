b6e0407a8c2c692e10c4dc1f25cf3ff55454bfaa
Jetross <jetrossneri07@gmail.com>
2025-04-08 14:06:23 +0800

    added postgresql to settings.py

--- trading-app-backend/mysite/settings.py
+++ trading-app-backend/mysite/settings.py
@@ -79,8 +79,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trading_app',
+        'USER': 'postgres',
+        'PASSWORD': '#Pa33w0rd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
